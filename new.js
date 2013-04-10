dc0114f767ce77cf1d1a8aca964b30fa6066f7d1
DB <rickyunimail@gmail.com>
2013-04-10 20:43:32 +0100

    test
    test

--- new.js
+++ new.js
@@ -9,3 +9,11 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @grant none
 // ==/UserScript==
+
+//Avg between highest buy & Lowest sell
+highestbuy = $("a", "tr:nth-child(3) td.coinformat:nth-child(2)").text();
+highestbuy = highestbuy.replace(",", "");
+
+lowestsell = $("a", "tr:nth-child(3) td.coinformat:nth-child(6)").text();
+lowestsell = lowestsell.replace(",", "");
+avg = ((parseFloat(highestbuy) + parseFloat(lowestsell)) / 2).toFixed(6);
