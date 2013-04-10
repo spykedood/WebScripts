6ba59f127a6f6c15706d9252123e661438986554
DB <rickyunimail@gmail.com>
2013-04-10 20:37:03 +0100

    Fap
    derp

---
+++ new.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Collaboration, Synergy, Money.
+// @namespace   Ricky
+// @match       https://vircurex.com/orders*
+// @match       https://vircurex.com/welcome/index?alt=*
+// @include     https://vircurex.com/welcome/index?alt=*
+// @include     https://vircurex.com/orders?alt=*
+// @version     0.1
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @grant none
+// ==/UserScript==
