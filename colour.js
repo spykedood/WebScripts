dfdef4b61a1f3c320f664a148c68a715e18cef22
dickbutt <rickyunimail@gmail.com>
2013-04-12 16:13:09 +0200

    Working buy/sell price function
    Yeah buddy!

--- colour.js
+++ colour.js
@@ -26,23 +26,18 @@ avg = ((parseFloat(highestbuy) + parseFloat(lowestsell)) / 2).toFixed(6);
 // Colorizes background depending on percentage difference
 function myFunction(a,b,c)
 {
+//Escuse the terrible variable naming plox
 	 if (c >= 0.00 && c < 10.00) {
-            //$(a).style.background-Color = b[0];
 			$(a).css({ "background-color": b[0] });
     } else if (c >= 10.00 && c < 20.00) {
-           // $(a).style.background-Color = b[1];
 			$(a).css({ "background-color": b[1] });
     } else if (c >= 20.00 && c < 30.00) {
-          // $(a).style.background-Color = b[2];
 			$(a).css({ "background-color": b[2] });
     } else if (c >= 30.00 && c < 50.00) {
-           // $(a).style.background-Color = b[3];
 			$(a).css({ "background-color": b[3] });
     } else if (c >= 50.00 && c < 75.00) {
-           // $(a).style.background-Color = b[4];
 			$(a).css({ "background-color": b[4] });
     } else if (c >= 75.00) {
-           // $(a).style.background-Color = b[5];
 			$(a).css({ "background-color": b[5] });
     } else {
 			alert('derp');
@@ -66,21 +61,13 @@ $(document).ready(function () {
         var sellcolour = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(6)')[0];
 
         //Partially finished buy colour ranking system
-		var BuyGradient = ['"#00FF33"','"#00CC33"','"#009933"','"#006633"','"#003333"','"#000033"'];
-			//Alerts work properly, so it isnt the array that's the problem.
-		    //alert(BuyGradient[0]);
-			//alert(BuyGradient[1]);
-			//alert(BuyGradient[2]);
-			//alert(BuyGradient[3]);
-			//alert(BuyGradient[4]);
-			//alert(BuyGradient[5]);
-		//var SellGradient = ['"#CCFF00"','"#CCCC00"','"#CC9900"','"#CC6600"','"#CC3300"','"#CC0000"'];
+		var BuyGradient = ['#00FF33','#00CC33','#009933','#006633','#003333','#000033'];
+		var SellGradient = ['#CCFF00','#CCCC00','#CC9900','#CC6600','#CC3300','#CC0000'];
 
-		 var pctDiff = (Math.abs(((buy / avg) * 100) - 100)).toFixed(2);
+		 var pctDiffbuy = (Math.abs(((buy / avg) * 100) - 100)).toFixed(2);
+		 var pctDiffsell = (Math.abs(((sell / avg) * 100) - 100)).toFixed(2);
 
-        myFunction(buycolour, BuyGradient, pctDiff);
-
-        //Partially finished sell colour ranking system
-        //colorizePending(sell, sellcolour, SellGradient);
+        myFunction(buycolour, BuyGradient, pctDiffbuy);
+		myFunction(sellcolour, SellGradient, pctDiffsell);
     }
 });
