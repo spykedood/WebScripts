cc6ab5e67f173d82dc68ec82b0adbb9270087606
dickbutt <rickyunimail@gmail.com>
2013-04-15 18:00:14 +0200

    Added new stuff
    Whole bunch of new stuff, inc grabbing which coin alt/base from url.

--- colour.js
+++ colour.js
@@ -5,10 +5,29 @@
 // @match       https://vircurex.com/welcome/index?alt=*
 // @include     https://vircurex.com/welcome/index?alt=*
 // @include     https://vircurex.com/orders?alt=*
-// @version     0.1
+// @version     0.15
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @grant none
 // ==/UserScript==
+
+
+//Not working yet - pass through the quantities when base/alt coin is detected in url!
+function colorize(qty, qtycolor, x) {
+    //colour coding the quantities of recent orders
+    if (qty > x[0] && qty < x[1]) {
+        $(qtycolor).css({ "background-color": "#F0F0F0" });
+    } else if (qty >= x[1] && qty < x[2]) {
+        $(qtycolor).css({ "background-color": "#D8D8D8" });
+    } else if (qty >= x[2] && qty < x[3]) {
+        $(qtycolor).css({ "background-color": "#B8B8B8" });
+    } else if (qty >= x[3] && qty < x[4]) {
+        $(qtycolor).css({ "background-color": "#787878" });
+    } else if (qty >= x[4] && qty < x[5]) {
+        $(qtycolor).css({ "background-color": "#585858" });
+    } else {
+        $(qtycolor).css({ "background-color": "#404040" });
+    }
+}
 
 // Colorizes background depending on percentage difference
 function myFunction(a,b,c)
@@ -31,12 +50,38 @@ function myFunction(a,b,c)
     }
 }
 
-function myFunction2(a,b)
+function valueAndCell(a,b,c,d)
 {
+if (c === 1){
         var cellvalue = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(' + b + ')').text();
         cellvalue = cellvalue.replace(",", "");
-        cellvalue = (parseFloat(cellvalue)).toFixed(6);
+        cellvalue = (parseFloat(cellvalue)).toFixed(' + d + ');
 		return cellvalue;
+		}
+else {
+         var cell = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(' + b + ')')[0];
+		return cell;
+}
+}
+
+function myFunction3(a,b,c,d)
+{
+//Escuse the terrible variable naming plox
+	 if (a > b) {
+			$(c).css({ "background-color": d[0] });
+    } else if (a < b) {
+			$(c).css({ "background-color": d[1] });
+    } else {
+			$(c).css({ "background-color": d[2] });
+    } 
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
 }
 
 $(document).ready(function () {
@@ -47,6 +92,8 @@ setTimeout("location.reload();",90000);
 var avg = 0;
 var highestbuy = 0;
 var lowestsell = 0;
+var recentHigh = 0;
+var recentLow = 0;
 
 //Avg between highest buy & Lowest sell
 highestbuy = $("a", "tr:nth-child(3) td.coinformat:nth-child(2)").text();
@@ -57,30 +104,48 @@ lowestsell = lowestsell.replace(",", "");
 avg = ((parseFloat(highestbuy) + parseFloat(lowestsell)) / 2).toFixed(6);
 
     for (var a = 3; a < 23; a++) {
-        //buy
-        //var buy = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(2)').text();
-        //buy = buy.replace(",", "");
-        //buy = (parseFloat(buy)).toFixed(6);
-        //sell
-        //var sell = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(6)').text();
-        //sell = sell.replace(",", "");
-        //sell = (parseFloat(sell)).toFixed(6);
-
-	var buy = myFunction2(a,2);
-	var sell = myFunction2(a,6);
-      
-        //var colours
-        var buycolour = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(2)')[0];
-        var sellcolour = $('.mainwindow .mylists tr:nth-child(' + a + ') td.coinformat:nth-child(6)')[0];
+		//Grabbing buy/sell/recent values
+		var buy = valueAndCell(a,2,1, 6);
+		var sell = valueAndCell(a,6,1, 6);
+		var recent = valueAndCell(a,11,1, 6);
+		
+		//Grabbing the cell locations of buy/sell/recent to colour.
+		var buycolour = valueAndCell(a,2,0,0);
+		var sellcolour = valueAndCell(a,6,0,0);
+		var recentcolour = valueAndCell(a,11,0,0);
+		
+		//(a,b,c) a=row b=td column c=(1=grab value.  0=grab cell) d=to fixed 4/6.
+		//Could replace c with True/False or Value/Cell to make it more readable!
+		//Grabbing the cell location of buy/sell/recent top
+		var buytop = valueAndCell(1,2,0, 0);
+		var selltop = valueAndCell(1,6,0, 0);
+		var recenttop = valueAndCell(1,11,0, 0);
+		
+		//Quantities; same as above!
+		var sellquantities = valueAndCell(a,5,1, 4);
+		var buyquantities = valueAndCell(a,1,1, 4);
+		var recentquantities = valueAndCell(a,10,1, 4);
+		
+		//Grab the alt/base from the url to determine the quantities in the page
+		var alt = getUrlVars()["alt"];
+		var base = getUrlVars()["base"];
+		//alert(alt);
+		//alert(base);
+		//Example of buyQtyArr - make more for each type of coin.
+		var buyQtyArr = ['0','100','200','300','400','500']
 
         //Partially finished buy colour ranking system
 		var BuyGradient = ['#00FF33','#00CC33','#009933','#006633','#003333','#000033'];
 		var SellGradient = ['#CCFF00','#CCCC00','#CC9900','#CC6600','#CC3300','#CC0000'];
+		var RecentGradient = ['#006600','#B80000 ','#FF9900'];
 
-		 var pctDiffbuy = (Math.abs(((buy / avg) * 100) - 100)).toFixed(2);
-		 var pctDiffsell = (Math.abs(((sell / avg) * 100) - 100)).toFixed(2);
+		var pctDiffbuy = (Math.abs(((buy / avg) * 100) - 100)).toFixed(2);
+		var pctDiffsell = (Math.abs(((sell / avg) * 100) - 100)).toFixed(2);
 
         myFunction(buycolour, BuyGradient, pctDiffbuy);
 		myFunction(sellcolour, SellGradient, pctDiffsell);
+		myFunction3(recent, avg, recentcolour, RecentGradient);
+
+		//myFunction4(recentHigh,recentLow,recentcolour);
     }
 });
