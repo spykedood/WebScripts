9e9f374adbe142bf2387a80906ec358b96ed3d29
dickbutt <rickyunimail@gmail.com>
2013-04-18 14:52:07 +0200

    Partially fixed

--- Universal functions.js
+++ Universal functions.js
@@ -177,7 +177,14 @@ function ResizeGraph() {
 // Colors buy/sell PRICE cells
 function ColorOrderPriceCells() {
     for (var a = 3; a < 23; a++) {
+        //Grabbing buy/sell/recent quantity values
+        //Temporarily keep for future purposes
+        //var buyqty = getHyperCellValue(a, 1);
+        //var sellqty = getHyperCellValue(a, 5);
+        //var recentqty = getCellValue(a, 10);
+        
         //Grabbing buy/sell/recent values
+
         //a=Location ie (.mainwindow .mylists) or ('a').
         //b=Row number (just pass through variable "a" from loop!).
         //c=Either ".coinformat" or "".
@@ -305,57 +312,58 @@ function supports_html5_storage() {
 
 var Cell = {
 
-//a=Location ie (.mainwindow .mylists) or ('a').
-//b=Row number (just pass through variable "a" from loop!).
-//c=Either ".coinformat" or "".
-//d=td nth child number.
-//e=to fixed number (4/6)
-Cellvalue: function(a, b, c, d, e) {
-    var cellvalue = $(a, 'tr:nth-child(' + b + ')', ' td' + c + ':nth-child(' + d + ')').text();
-    cellvalue = cellvalue.replace(",", "");
-    cellvalue = (parseFloat(cellvalue)).toFixed(e);
-    return cellvalue;
-}
+    //a=Location ie (.mainwindow .mylists) or ('a').
+    //b=Row number (just pass through variable "a" from loop!).
+    //c=Either ".coinformat" or "".
+    //d=td nth child number.
+    //e=to fixed number (4/6)
+    Cellvalue: function(a, b, c, d, e) {
+        var cellvalue = $(a, 'tr:nth-child(' + b + ')', ' td' + c + ':nth-child(' + d + ')').text();
+        cellvalue = cellvalue.replace(",", "");
+        cellvalue = (parseFloat(cellvalue)).toFixed(e);
+        alert(cellValue);
+        return cellvalue;
+    },
 
-//a=Location ie (.mainwindow .mylists) or ('a').
-//b=Row number (just pass through variable "a" from loop!).
-//c=Either ".coinformat" or "".
-//d=td nth child number.
-getCell: function(a, b, c, d) {
-    var cell = $(a, 'tr:nth-child(' + b + ')', ' td' + c + ':nth-child(' + d + ')')[0];
-    return cell;
-}
+    //a=Location ie (.mainwindow .mylists) or ('a').
+    //b=Row number (just pass through variable "a" from loop!).
+    //c=Either ".coinformat" or "".
+    //d=td nth child number.
+    getCell: function(a, b, c, d) {
+        var cell = $(a, 'tr:nth-child(' + b + ')', ' td' + c + ':nth-child(' + d + ')')[0];
+        return cell;
+    },
 
-Colours: function(a, b, c, d) {
-    if ((b >= c[0]) && (b < c[1])) {
-        $(a).css({ "background-color": d[0] });
-        //alert(b + ' '+ c[0] + ' '+ c[1]);
-    } else if ((b >= c[1]) && (b < c[2])) {
-        $(a).css({ "background-color": d[1] });
-        //alert(b + ' '+ c[1] + ' '+ c[2]);
-    } else if ((b >= c[2]) && (b < c[3])) {
-        $(a).css({ "background-color": d[2] });
-        //alert(b + ' '+ c[2] + ' '+ c[3]);
-    } else if ((b >= c[3]) && (b < c[4])) {
-        $(a).css({ "background-color": d[3] });
-        //alert(b + ' '+ c[3] + ' '+ c[4]);
-    } else if ((b >= c[4]) && (b < c[5])) {
-        $(a).css({ "background-color": d[4] });
-        //alert(b + ' '+ c[4] + ' '+ c[5]);
-    } else if (b >= c[5]) {
-        $(a).css({ "background-color": d[5] });
-        //alert(b + ' '+ c[5]);
-    } else {
-        //alert('derp');
+    Colours: function(a, b, c, d) {
+        if ((b >= c[0]) && (b < c[1])) {
+            $(a).css({ "background-color": d[0] });
+            //alert(b + ' '+ c[0] + ' '+ c[1]);
+        } else if ((b >= c[1]) && (b < c[2])) {
+            $(a).css({ "background-color": d[1] });
+            //alert(b + ' '+ c[1] + ' '+ c[2]);
+        } else if ((b >= c[2]) && (b < c[3])) {
+            $(a).css({ "background-color": d[2] });
+            //alert(b + ' '+ c[2] + ' '+ c[3]);
+        } else if ((b >= c[3]) && (b < c[4])) {
+            $(a).css({ "background-color": d[3] });
+            //alert(b + ' '+ c[3] + ' '+ c[4]);
+        } else if ((b >= c[4]) && (b < c[5])) {
+            $(a).css({ "background-color": d[4] });
+            //alert(b + ' '+ c[4] + ' '+ c[5]);
+        } else if (b >= c[5]) {
+            $(a).css({ "background-color": d[5] });
+            //alert(b + ' '+ c[5]);
+        } else {
+            //alert('derp');
+        }
+    },
+
+    recBuySell: function(text, cell) {
+        if (text === "Buy") {
+            $(cell).css({ "background-color": "#006600" });
+        } else {
+            $(cell).css({ "background-color": "#B80000" });
+        }
     }
-}
-
-recBuySell: function(text, cell) {
-    if (text === "Buy") {
-        $(cell).css({ "background-color": "#006600" });
-    } else {
-        $(cell).css({ "background-color": "#B80000" });
-    }
-}
 
 }
