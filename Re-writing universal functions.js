e1036da5f9cc80c43064bb4c773440858934e9c6
dickbutt <rickyunimail@gmail.com>
2013-04-19 17:49:18 +0200

    Changed the functions.
    Now can more easily specify location of what value/cell you want to grab.

--- Re-writing universal functions.js
+++ Re-writing universal functions.js
@@ -11,6 +11,11 @@
 // @grant none
 // ==/UserScript==
 
+/*
+NASA is furnishing this item "as is". NASA does not provide any warranty of the item whatsoever, whether express, implied, or statutory, including, but not limited to, any warranty of merchantability or fitness for a particular purpose or any warranty that the contents of the item will be error-free.
+In no respect shall NASA incur any liability for any damages, including, but limited to, direct, indirect, special, or consequential damages arising out of, resulting from, or any way connected to the use of the item, whether or not based upon warranty, contract, tort, or otherwise; whether or not injury was sustained by persons or property or otherwise; and whether or not loss was sustained from, or arose out of, the results of, the item, or any services that may be provided by NASA.
+*/
+
 function init() {
     // All initialization goes here.
     var highestbuy = $('a', 'tr:nth-child(3) td.coinformat:nth-child(2)').text();
@@ -48,38 +53,38 @@ $(document).ready(function ()
             //c=Either '.coinformat' or ''.
             //d=td nth child number.
             //e=to fixed number (4/6)
-            var buy = CellValue('text', i, 2);
-            var sell = CellValue('text', i, 6);
-            var recent = CellValue('text', i, 11);
+            var buy = CellValue('main', 'text', i, 2, 6);
+            var sell = CellValue('main', 'text', i, 6, 6);
+            var recent = CellValue('main', 'text', i, 11, 6);
 
             //Grabbing buy/sell/recent quantity values
-            var buyqty = CellValue('text', i, 1);
-            var sellqty = CellValue('text', i, 5);
-            var recentqty = CellValue('text', i, 10);
+            var buyqty = CellValue('main', 'text', i, 1, 6);
+            var sellqty = CellValue('main', 'text', i, 5, 6);
+            var recentqty = CellValue('main', 'text', i, 10, 6);
 
             //Grabbing btc values
-            var btcbuyqty = CellValue('text', i, 3);
-            var btcsellqty = CellValue('text', i, 7);
-            var btcrecqty = CellValue('text', i, 12);
+            var btcbuyqty = CellValue('main', 'text', i, 3, 6);
+            var btcsellqty = CellValue('main', 'text', i, 7, 6);
+            var btcrecqty = CellValue('main', 'text', i, 12, 6);
             
             //Grabbing btc cells
             //a=Location ie (.mainwindow .mylists) or (.infobox) (Where you want)
             //b=Row number (just pass through variable 'a' from loop!).
             //c=Either '.coinformat' or ''.
             //d=td nth child number.
-            var btcBuyQtyCell = getCell( 'text', i, 3);
-            var btcSellQtyCell = getCell( 'text', i, 7);
-            var btcRecQtyCell = getCell( 'text', i, 12);
+            var btcBuyQtyCell = getCell('main', 'text', i, 3);
+            var btcSellQtyCell = getCell('main', 'text', i, 7);
+            var btcRecQtyCell = getCell('main', 'text', i, 12);
 
             //Grabbing the buy/sell/recent quantity cells
-            var buyqtycell = getCell( 'text', i, 1);
-            var sellqtycell = getCell( 'text', i, 5);
-            var recentqtycell = getCell( 'text', i, 10);
+            var buyqtycell = getCell('main', 'text', i, 1);
+            var sellqtycell = getCell('main', 'text', i, 5);
+            var recentqtycell = getCell('main', 'text', i, 10);
 
             //Grabbing the cell locations of buy/sell/recent to colour
-            var buycolour = getCell( 'text', i, 2);
-            var sellcolour = getCell( 'text', i, 6);
-            var recentcolour = getCell( 'text', i, 11);
+            var buycolour = getCell('main', 'text', i, 2);
+            var sellcolour = getCell('main', 'text', i, 6);
+            var recentcolour = getCell('main', 'text', i, 11);
             //Recent value ('buy/sell') & recent cell
             var recentOrdVal = $('.mainwindow .mylists tr:nth-child(' + i + ') td:nth-child(13)').text();
             var recentOrdCell = $('.mainwindow .mylists tr:nth-child(' + i + ') td:nth-child(13)')[0];
@@ -117,23 +122,34 @@ $(document).ready(function ()
 });
 
 
-//a=Location ie (.mainwindow .mylists) or ('a').
-//b=Row number (just pass through variable 'a' from loop!).
+//a=Location mainwindow or right (hand side of page).
+//b=Value text type (hyperlink or plaintext).
 //c=Either '.coinformat' or ''.
 //d=td nth child number.
 //e=to fixed number (4/6)
-function CellValue(a, b, c) {
+function CellValue(a, b, c, d, e) {
     var cellvalue;
-        if (a === 'link') {
-            cellvalue = $('a', 'tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')').text();
-        } else if (a === 'text') {
-            cellvalue = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')').text();
+    if (a === 'mainwindow') {
+        function derp(b, c, d, e);
+    } else if (a === 'right') {
+        function derp(b, c, d, e);
+    } else {
+        alert("Value location derp!");
+    }
+
+    function derp(b, c, d, e){
+        if (b === 'link') {
+            cellvalue = $('a', 'tr:nth-child(' + c + ') td.coinformat:nth-child(' + d + ')').text();
+        } else if (b === 'text') {
+            cellvalue = $('.mainwindow .mylists tr:nth-child(' + c+ ') td.coinformat:nth-child(' + d + ')').text();
         } else {
-            alert("value derp");
+            alert("Value text type derp! text or hyperlink!");
         }
+    }
+
     cellvalue = cellvalue.replace(',', '');
-    cellvalue = (parseFloat(cellvalue)).toFixed(6);
-        alert(cellvalue);
+    cellvalue = (parseFloat(cellvalue)).toFixed(e);
+    //alert(cellvalue);
     return cellvalue;
 }
 
@@ -141,13 +157,28 @@ function CellValue(a, b, c) {
 //b=Row number (just pass through variable 'a' from loop!).
 //c=Either '.coinformat' or ''.
 //d=td nth child number.
-function getCell(a, b, c) {
+function getCell(a, b, c, d) {
+    var cellvalue;
+    if (a === 'mainwindow') {
+        function derp(b, c, d);
+    } else if (a === 'right') {
+        function derp(b, c, d);
+    } else {
+        alert("Value location derp!");
+    }
+
+
+    //AM I DECLARING CELL PROPERLY?!?!?!?! WAT IS THIS?! AAHH!
      var cell;
-        if (a === 'link') {
-            cell = $('.mainwindow .mylists tr:nth-child(' + b + ')', 'td:nth-child(' + c + ')')[0];
-        } else if (a === 'text') {
-            cell = $('.mainwindow .mylists tr:nth-child(' + b + ')', 'td.coinformat:nth-child(' + c + ')')[0];
+    //SEE THAT THERE ABOVE THIS AND BELOW THAT?! WUT!
+
+    function derp(b, c, d){    
+        if (b === 'link') {
+            cell = $('.mainwindow .mylists tr:nth-child(' + c + ')', 'td:nth-child(' + d + ')')[0];
+        } else if (b === 'text') {
+            cell = $('.mainwindow .mylists tr:nth-child(' + c + ')', 'td.coinformat:nth-child(' + d + ')')[0];
         }
+    }
     return cell;
 }
 
