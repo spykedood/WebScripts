f51e76298a38469c5f3170b5e2fed2c155377916
dickbutt <rickyunimail@gmail.com>
2013-04-22 00:28:32 +0200

    still broken
    It's to do with either the cell grabbing function or the colours function.

--- Re-writing universal functions.js
+++ Re-writing universal functions.js
@@ -79,6 +79,7 @@ $(document).ready(function ()
             var buycolour = getCell('mainwindow', 'text', i, 2);
             var sellcolour = getCell('mainwindow', 'text', i, 6);
             var recentcolour = getCell('mainwindow', 'text', i, 11);
+
             //Recent value ('buy/sell') & recent cell
             var recentOrdVal = $('.mainwindow .mylists tr:nth-child(' + i + ') td:nth-child(13)').text();
             var recentOrdCell = $('.mainwindow .mylists tr:nth-child(' + i + ') td:nth-child(13)')[0];
@@ -98,10 +99,11 @@ $(document).ready(function ()
             //Working
             var pctDiffbuy = (Math.abs(((buy / avg) * 100) - 100)).toFixed(2);
             var pctDiffsell = (Math.abs(((sell / avg) * 100) - 100)).toFixed(2);
-            alert("Loop end!");
-    }
 
-                //Calling function to colour the buy/sell/recent prices
+            alert("Loop end!");
+    
+
+                    //Calling function to colour the buy/sell/recent prices
             Colours(buycolour, pctDiffbuy, pctDiffArr, BuyGradient);
             Colours(sellcolour, pctDiffsell, pctDiffArr, SellGradient);
             //
@@ -114,6 +116,7 @@ $(document).ready(function ()
             Colours(btcRecQtyCell, btcrecqty, btcQtyArr, QtyGradient);
 
             recBuySell(recentOrdVal, recentOrdCell);
+    }
 });
 
 
@@ -123,7 +126,7 @@ $(document).ready(function ()
 //d=td nth child number.
 //e=to fixed number (4/6)
 function CellValue(a, b, c, d) {
-    var cellvalue;
+    var cellvalue = 0;
     if (a === 'mainwindow') {
         derp(b, c, d);
     } else if (a === 'right') {
@@ -164,7 +167,7 @@ function getCell(a, b, c, d) {
 
 
     //AM I DECLARING CELL PROPERLY?!?!?!?! WAT IS THIS?! AAHH!
-     var cell;
+     var cell = 0;
     //SEE THAT THERE ABOVE THIS AND BELOW THAT?! WUT!
 
     function derp(b, c, d){    
@@ -179,23 +182,23 @@ function getCell(a, b, c, d) {
 
 function Colours(a, b, c, d) {
     if ((b >= c[0]) && (b < c[1])) {
-        $(a).css( 'background-color': d[0] );
-        //alert(b + ' '+ c[0] + ' '+ c[1]);
+        $(a).css({ 'background-color': d[0] });
+            alert(b + ' '+ c[0] + ' '+ c[1]);
     } else if ((b >= c[1]) && (b < c[2])) {
-        $(a).css( 'background-color': d[1] );
-        //alert(b + ' '+ c[1] + ' '+ c[2]);
+        $(a).css({ 'background-color': d[1] });
+            alert(b + ' '+ c[1] + ' '+ c[2]);
     } else if ((b >= c[2]) && (b < c[3])) {
-        $(a).css( 'background-color': d[2] );
-        //alert(b + ' '+ c[2] + ' '+ c[3]);
+        $(a).css({ 'background-color': d[2] });
+            alert(b + ' '+ c[2] + ' '+ c[3]);
     } else if ((b >= c[3]) && (b < c[4])) {
-        $(a).css( 'background-color': d[3] );
-        //alert(b + ' '+ c[3] + ' '+ c[4]);
+        $(a).css({ 'background-color': d[3] });
+            alert(b + ' '+ c[3] + ' '+ c[4]);
     } else if ((b >= c[4]) && (b < c[5])) {
-        $(a).css( 'background-color': d[4] );
-        //alert(b + ' '+ c[4] + ' '+ c[5]);
+        $(a).css({ 'background-color': d[4] });
+            alert(b + ' '+ c[4] + ' '+ c[5]);
     } else if (b >= c[5]) {
-        $(a).css( 'background-color': d[5] );
-        //alert(b + ' '+ c[5]);
+        $(a).css({ 'background-color': d[5] });
+            alert(b + ' '+ c[5]);
     } else {
         alert('derp');
     }
