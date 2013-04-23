541d5a5b52b1234b5afe4b6c2332e665498e157c
dickbutt <rickyunimail@gmail.com>
2013-04-24 00:59:52 +0200

    Modularized the functions LIKE A BOSS!
    Simply conquering shit as i see fit.

--- Re-written universal functions.js
+++ Re-written universal functions.js
@@ -5,7 +5,7 @@
 // @match       https://vircurex.com/welcome/index?alt=*
 // @include     https://vircurex.com/welcome/index?alt=*
 // @include     https://vircurex.com/orders?alt=*
-// @version     0.16
+// @version     0.165
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require     http://code.jquery.com/ui/1.10.2/jquery-ui.js
 // @grant none
@@ -49,44 +49,41 @@ $(document).ready(function ()
             //b=Value text type (hyperlink or plaintext).
             //c=Either '.coinformat' or ''.
             //d=td nth child number.
-            var buy = CellValue('mainwindow', 'link', i, 2);
-            var sell = CellValue('mainwindow', 'link', i, 6);
-            var recent = CellValue('mainwindow', 'text', i, 11);
+            var buy = Vircurex.derp('link', i, 2);
+            var sell = Vircurex.derp('link', i, 6);
+            var recent = Vircurex.derp('text', i, 11);
 
             //Grabbing buy/sell/recent quantity values
-            var buyqty = CellValue('mainwindow', 'link', i, 1);
-            var sellqty = CellValue('mainwindow', 'link', i, 5);
-            var recentqty = CellValue('mainwindow', 'text', i, 10);
+            var buyqty = Vircurex.derp('link', i, 1);
+            var sellqty = Vircurex.derp('link', i, 5);
+            var recentqty = Vircurex.derp('text', i, 10);
 
             //Grabbing btc values
-            var btcbuyqty = CellValue('mainwindow', 'text', i, 3);
-            var btcsellqty = CellValue('mainwindow', 'text', i, 7);
-            var btcrecqty = CellValue('mainwindow', 'text', i, 12);
+            var btcbuyqty = Vircurex.derp('text', i, 3);
+            var btcsellqty = Vircurex.derp('text', i, 7);
+            var btcrecqty = Vircurex.derp('text', i, 12);
             
             //Grabbing btc cells
-            //a=Location ie (.mainwindow .mylists) or (.infobox) (Where you want)
-            //b=Row number (just pass through variable 'a' from loop!).
-            //c=Either '.coinformat' or ''.
-            //d=td nth child number.
-            var btcBuyQtyCell = getCell('mainwindow', i, 3);
-            var btcSellQtyCell = getCell('mainwindow', i, 7);
-            var btcRecQtyCell = getCell('mainwindow', i, 12);
+            var btcBuyQtyCell = Vircurex.getCell('mainwindow', i, 3);
+            var btcSellQtyCell = Vircurex.getCell('mainwindow', i, 7);
+            var btcRecQtyCell = Vircurex.getCell('mainwindow', i, 12);
 
             //Grabbing the buy/sell/recent quantity cells
-            var buyqtycell = getCell('mainwindow', i, 1);
-            var sellqtycell = getCell('mainwindow', i, 5);
-            var recentqtycell = getCell('mainwindow', i, 10);
+            var buyqtycell = Vircurex.getCell('mainwindow', i, 1);
+            var sellqtycell = Vircurex.getCell('mainwindow', i, 5);
+            var recentqtycell = Vircurex.getCell('mainwindow', i, 10);
 
             //Grabbing the cell locations of buy/sell/recent to colour
-            var buycolour = getCell('mainwindow', i, 2);
-            var sellcolour = getCell('mainwindow', i, 6);
-            var recentcolour = getCell('mainwindow', i, 11);
-            var recTime = getCell('mainwindow', i, 9);
+            var buycolour = Vircurex.getCell('mainwindow', i, 2);
+            var sellcolour = Vircurex.getCell('mainwindow', i, 6);
+            var recentcolour = Vircurex.getCell('mainwindow', i, 11);
+            var recTime = Vircurex.getCell('mainwindow', i, 9);
 
-            //Grabbing the 3 top cells
-            //var buytop = getCell('maintop', 1, 1);
-            //var selltop = getCell('maintop', 1, 2);
-            //var rectop = getCell('maintop', 1, 3);
+                //Grabbing the 3 top cells
+                //var buytop = Vircurex.getCell('maintop', 1, 1);
+                //var selltop = Vircurex.getCell('maintop', 1, 2);
+                //rectop doesn't work ;_;
+                //var rectop = Vircurex.getCell('maintop', 1, 3);
 
             //Recent value ('buy/sell') & recent cell
             var recentOrdVal = $('.mainwindow .mylists tr:nth-child(' + i + ') td:nth-child(13)').text();
@@ -107,127 +104,134 @@ $(document).ready(function ()
             var pctDiffsell = (Math.abs(((sell / avg) * 100) - 100)).toFixed(2);
             //alert(pctDiffsell);
 
-            //alert("Loop end!");
-    
-
             //Calling function to colour the buy/sell/recent prices
-            Colours(buycolour, pctDiffbuy, pctDiffArr, BuyGradient);
-            Colours(sellcolour, pctDiffsell, pctDiffArr, SellGradient);
+            Vircurex.Colours(buycolour, pctDiffbuy, pctDiffArr, BuyGradient);
+            Vircurex.Colours(sellcolour, pctDiffsell, pctDiffArr, SellGradient);
             //---//
-            Colours(buyqtycell, btcbuyqty, btcQtyArr, QtyGradient);
-            Colours(sellqtycell, btcsellqty, btcQtyArr, QtyGradient);
-            Colours(recentqtycell, btcrecqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(buyqtycell, btcbuyqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(sellqtycell, btcsellqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(recentqtycell, btcrecqty, btcQtyArr, QtyGradient);
             //---//
-            Colours(btcSellQtyCell, btcsellqty, btcQtyArr, QtyGradient);
-            Colours(btcBuyQtyCell, btcbuyqty, btcQtyArr, QtyGradient);
-            Colours(btcRecQtyCell, btcrecqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(btcSellQtyCell, btcsellqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(btcBuyQtyCell, btcbuyqty, btcQtyArr, QtyGradient);
+            Vircurex.Colours(btcRecQtyCell, btcrecqty, btcQtyArr, QtyGradient);
 
             //Colouring in recent "buy/sell"
-            recBuySell(recentOrdVal, recentOrdCell);
+            Vircurex.recBuySell(recentOrdVal, recentOrdCell);
 
             //Muh-Fuggan strike throughs!
-            Strike(btcbuyqty, buyqtycell, btcBuyQtyCell, buycolour);
-            Strike(btcsellqty, sellqtycell, btcSellQtyCell, sellcolour);
-            Strike(btcrecqty, recentqtycell, btcRecQtyCell, recentcolour);
+            Vircurex.Strike(btcbuyqty, buyqtycell, btcBuyQtyCell, buycolour);
+            Vircurex.Strike(btcsellqty, sellqtycell, btcSellQtyCell, sellcolour);
+            Vircurex.Strike(btcrecqty, recentqtycell, btcRecQtyCell, recentcolour);
+            //alert("Loop end!");
+
     }
 });
 
+var Vircurex = {
+    //a=Location mainwindow or right (hand side of page).
+    //b=Value text type (hyperlink or plaintext).
+    //c=Either '.coinformat' or ''.
+    //d=td nth child number.
+    //CellValue: function(a, b, c, d) {
+        //if (a === 'mainwindow') {
+            //Mainwindow = left content
+            //These sections could add ability to simplify grabbing cellvalue
+            //For now they're fucking useless.
+            //Vircurex.derp(b, c, d);
+        //} else if (a === 'right') {
+            //Right = right... noob.
+            //Same as above.
+            //Vircurex.derp(b, c, d);
+        //} else {
+            //If you called this function wrong, this alert will be called.
+            //alert("Value location derp!");
+        //}
+    //},
 
-//a=Location mainwindow or right (hand side of page).
-//b=Value text type (hyperlink or plaintext).
-//c=Either '.coinformat' or ''.
-//d=td nth child number.
-//e=to fixed number (4/6)
-function CellValue(a, b, c, d) {
-    var cellvalue = 0;
-    if (a === 'mainwindow') {
-        derp(b, c, d);
-    } else if (a === 'right') {
-        derp(b, c, d);
-    } else {
-        alert("Value location derp!");
-    }
+    derp: function(a, b, c) {
+        var cellvalue = 0;
 
-    function derp(b, c, d){
-        if (b === 'link') {
-            cellvalue = $('a', 'tr:nth-child(' + c + ') td.coinformat:nth-child(' + d + ')').text();
-        } else if (b === 'text') {
-            cellvalue = $('.mainwindow .mylists tr:nth-child(' + c + ') td.coinformat:nth-child(' + d + ')').text();
-        } else {
-            alert("Value text type derp! text or hyperlink!");
-        }
-    }
-
-    cellvalue = cellvalue.replace(',', '');
-    cellvalue = (parseFloat(cellvalue)).toFixed(6);
-    //alert(cellvalue);
-    return cellvalue;
-}
-
-function Strike(a, b, c, d) {
-            //Strike through Script
-            if (a < 0.020000) {
-                //Strikes through the recent orders that are too small
-                $(b).css('textDecoration', 'line-through');
-                $(c).css('textDecoration', 'line-through'); 
-                $(d).css('textDecoration', 'line-through'); 
+            if (a === 'link') {
+                cellvalue = $('a', 'tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')').text();
+            } else if (a === 'text') {
+                cellvalue = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')').text();
+            } else {
+                alert("Value text type derp! text or hyperlink!");
             }
-}
 
-//a=Location ie (.mainwindow .mylists) or ('a').
-//b=Row number (just pass through variable 'a' from loop!).
-//c=Either '.coinformat' or ''.
-//d=td nth child number.
-function getCell(a, b, c) {
-    var cell = 'test';
-    //var cell = 0;
-    //var cell;
-    if (a === 'mainwindow') {
-            cell = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')')[0];
-            //alert(cell);
-    } else if (a === 'right') {
-            cell = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')')[0];
-            //alert(cell);
-    } else if (a === 'maintop') {
-            cell = $('.mainwindow .mylists tr:nth-child(' + b + ') th:nth-child(' + c + ')')[0];
-            //alert(cell);
-    } else {
-        alert("Value location derp!");
+        cellvalue = cellvalue.replace(',', '');
+        cellvalue = (parseFloat(cellvalue)).toFixed(6);
+        //alert(cellvalue);
+        return cellvalue;
+    },
+
+    Strike: function(a, b, c, d) {
+                //Strike through Script
+                if (a < 0.020000) {
+                    //Strikes through the recent orders that are too small
+                    $(b).css('textDecoration', 'line-through');
+                    $(c).css('textDecoration', 'line-through'); 
+                    $(d).css('textDecoration', 'line-through'); 
+                }
+    },
+
+    //a=Location ie (.mainwindow .mylists) or ('a').
+    //b=Row number (just pass through variable 'a' from loop!).
+    //c=Either '.coinformat' or ''.
+    //d=td nth child number.
+    getCell: function(a, b, c) {
+        var cell = 'test';
+        //var cell = 0;
+        //var cell;
+        if (a === 'mainwindow') {
+                cell = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')')[0];
+                //alert(cell);
+        } else if (a === 'right') {
+                cell = $('.mainwindow .mylists tr:nth-child(' + b + ') td.coinformat:nth-child(' + c + ')')[0];
+                //alert(cell);
+        } else if (a === 'maintop') {
+                cell = $('.mainwindow .mylists tr:nth-child(' + b + ') th:nth-child(' + c + ')')[0];
+                //alert(cell);
+        } else {
+            alert("Value location derp!");
+        }
+        //alert(cell);
+        return cell;
+    },
+
+    Colours: function(a, b, c, d) {
+        if ((b >= c[0]) && (b < c[1])) {
+            $(a).css({ 'background-color': d[0] });
+                //alert(b + ' '+ c[0] + ' '+ c[1]);
+        } else if ((b >= c[1]) && (b < c[2])) {
+            $(a).css({ 'background-color': d[1] });
+                //alert(b + ' '+ c[1] + ' '+ c[2]);
+        } else if ((b >= c[2]) && (b < c[3])) {
+            $(a).css({ 'background-color': d[2] });
+                //alert(b + ' '+ c[2] + ' '+ c[3]);
+        } else if ((b >= c[3]) && (b < c[4])) {
+            $(a).css({ 'background-color': d[3] });
+                //alert(b + ' '+ c[3] + ' '+ c[4]);
+        } else if ((b >= c[4]) && (b < c[5])) {
+            $(a).css({ 'background-color': d[4] });
+                //alert(b + ' '+ c[4] + ' '+ c[5]);
+        } else if (b >= c[5]) {
+            $(a).css({ 'background-color': d[5] });
+                //alert(b + ' '+ c[5]);
+        } else {
+            alert('Well something broke..');
+        }
+    },
+
+    recBuySell: function(a, b) 
+        {
+            if (a === 'Buy') {
+                $(b).css({ 'background-color': '#006600' });
+                buysell++;
+            } else if (a === 'Sell') {
+                $(b).css({ 'background-color': '#B80000' });
+                buysell -= 1;
+            } 
     }
-    return cell;
-}
-
-function Colours(a, b, c, d) {
-    if ((b >= c[0]) && (b < c[1])) {
-        $(a).css({ 'background-color': d[0] });
-            //alert(b + ' '+ c[0] + ' '+ c[1]);
-    } else if ((b >= c[1]) && (b < c[2])) {
-        $(a).css({ 'background-color': d[1] });
-            //alert(b + ' '+ c[1] + ' '+ c[2]);
-    } else if ((b >= c[2]) && (b < c[3])) {
-        $(a).css({ 'background-color': d[2] });
-            //alert(b + ' '+ c[2] + ' '+ c[3]);
-    } else if ((b >= c[3]) && (b < c[4])) {
-        $(a).css({ 'background-color': d[3] });
-            //alert(b + ' '+ c[3] + ' '+ c[4]);
-    } else if ((b >= c[4]) && (b < c[5])) {
-        $(a).css({ 'background-color': d[4] });
-            //alert(b + ' '+ c[4] + ' '+ c[5]);
-    } else if (b >= c[5]) {
-        $(a).css({ 'background-color': d[5] });
-            //alert(b + ' '+ c[5]);
-    } else {
-        alert('derp');
-    }
-}
-
-function recBuySell(a, b) 
-    {
-        if (a === 'Buy') {
-            $(b).css({ 'background-color': '#006600' });
-            buysell += 1;
-        } else if (a === 'Sell') {
-            $(b).css({ 'background-color': '#B80000' });
-            buysell -= 1;
-        } 
-}
+};
