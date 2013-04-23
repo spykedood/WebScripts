46b87b61b6fb26fa8e875d2d06d2c40148d497c4
dickbutt <rickyunimail@gmail.com>
2013-04-23 17:24:32 +0200

    Small update
    made function to replace bottom colouring

--- MiscScripts.js
+++ MiscScripts.js
@@ -3,27 +3,27 @@ var balance = //Grab input from user
 var PreProfit = (LowestSell-HighestBuy) * Balance;
 var Cost = 0;
     for (var i = 3; i < 23; i++) {
-    	If (SellQuantity < 0.x) { //sellquantity being the quantity of btc being exchanged
-    		Cost += Sellquantity
-    	} else {
-    		var lowPostPrice = //This row's price
-    		i = 23; //break out of loop
-    	}
+      If (SellQuantity < 0.x) { //sellquantity being the quantity of btc being exchanged
+        Cost += Sellquantity
+      } else {
+        var lowPostPrice = //This row's price
+        i = 23; //break out of loop
+      }
 
-  	} //end of loop
+    } //end of loop
 var PostProfit = (((LowestSell-HighestBuy)*Balance) - PreProfit)-Cost;
 If ((PostProfit > 0) && (RecentBuys > RecentSells)) {
-	Alert("Worth spending" Cost "btc to raise price to" lowPostPrice "You'll earn" PostProfit);
+  Alert("Worth spending" Cost "btc to raise price to" lowPostPrice "You'll earn" PostProfit);
 } else {
-	alert("Not worth it.")
+  alert("Not worth it.")
 }
 
 
 //Website status Script
 //Instead of an alert, we could have red/green lights if it doesnt load/ doesloads.
 function reportError(xObj){  
-	locationName = xObj.alt;  
-	alert("Links on this page may not work since the server at " + locationName + " appears to be down!");  
+  locationName = xObj.alt;  
+  alert("Links on this page may not work since the server at " + locationName + " appears to be down!");  
 }  
 
 <img src="URL" alt="URL" style="width:0;height:0;visiblity:hidden;position:absolute;" onerror="reportError(this)">  
@@ -105,3 +105,18 @@ if(PPCProfit > 0) {
   } else {
   $(".Profit2").css({"background-color": "red"});
   }
+
+//Above made into a function
+function balancecolour(a,b) {
+if(a > 0) {
+  $(b).css({"background-color": "green"});
+  } else if (a === 0){
+  $(b).css({"background-color": "orange"});
+  } else {
+  $b).css({"background-color": "red"});
+  }
+}
+
+//Calling above function
+balancecolour(TRCProfit, ".Profit3"); //trcprofit
+balancecolour(PPCProfit, ".Profit2"); //ppcprofit
