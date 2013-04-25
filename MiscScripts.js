38adf06acd3fe47b206d0c8d90c86e095d0a80e0
dickbutt <rickyunimail@gmail.com>
2013-04-25 17:09:29 +0200

    Added new balance scripts
    Can identify all types of currency in user's balance.

--- MiscScripts.js
+++ MiscScripts.js
@@ -55,18 +55,32 @@ var InitTRC = 0.00105;
 var TRCProfit = 0;
 var TRCBalance = 0;
 
-AvgPPC = AvgVal(9);
-AvgTRC = AvgVal(11)
+AvgPPC = Balancebox.AvgVal(9);
 
-TRCBalance = balance(6);
-PPCBalance = balance(9);
+PPCBalance = Balancebox.balance(9);
 
-function balance(tr) {
+//Ammending HTML to the balance box section - will be a profitability calculator.
+//Quantity/Current Price/Price bought in at = +- profit infoboxes
+$('#balancebox').append('</br><table class=\"mylists\" style="font-size: 70%"><tr><td colspan=5><strong>Profit</strong></td></tr><tr><th></th><th>Balance</th><th>Currently</th><th>Bought @</th><th>Profit</th></tr><tr><td colspan=\"5\"></td></tr><tr><td>BTC</td><td class=\"BTCBalance\">64.48127185</td><td class=\"Current1\">TEST</td></tr><tr class=\"alt\"><td>PPC</td><td class=\"PPCBalance\">19,900.00000001</td><td class=\"Current2\">' + AvgPPC + '</td><td class=\"BoughtAt\">' + InitPPC + '</td><td class=\"Profit2\">' + PPCProfit + '</td></tr><tr><td>TRC</td><td class=\"TRCBalance\">4,975.00000000</td><td class=\"Current3\">' + AvgTRC + '</td><td class=\"BoughtAt3\">' + InitTRC + '</td><td class=\"Profit3\">' + TRCProfit + '</td></tr></table>');
+
+//Calling below function
+Balancebox.balancecolour(TRCProfit, ".Profit3"); //trcprofit
+Balancebox.balancecolour(PPCProfit, ".Profit2"); //ppcprofit
+
+//Beginning of modular function container
+var BalanceBox = {
+
+balance: function(tr) {
   var Balance = $("#balancebox .mylists tr:nth-child(" + tr + ") td.coinformat:nth-child(2)").text();
   return Balance;
-}
+},
 
-function AvgVal(tr) {
+balanceType: function(tr) {
+  var Balance = $("#balancebox .mylists tr:nth-child(" + tr + ") td.coinformat:nth-child(2)").text();
+  return Balance;
+},
+
+AvgVal: function(tr) {
 High = $("#exinfobox .mylists tr:nth-child(" + tr + ") td.coinformat:nth-child(2)").text();
 High = Cleanup(High);
 //alert(TRCHigh);
@@ -75,30 +89,27 @@ Low = Cleanup(Low);
 //alert(TRCLow);
 AvgVal = (((High) + (Low)) / 2).toFixed(6);
 return AvgVal;
-}
+},
 
-function CleanUp(Value){
+CleanUp: function(Value){
   Value = Value.replace(/,/g, '');
   parseFloat(Value);
   return Value;
-}
+},
 
-function Profit(Balance, AvgCal, InitCoinVal){
+Profit: function(Balance, AvgCal, InitCoinVal){
   Profit = ((Balance * AvgVal)-(Balance * InitCoinVal)).toFixed(6);
-}
+},
 
 //The above function will replace the following two lines.
 //TRCProfit = ((TRCBalance * AvgTRC)-(TRCBalance * InitTRC)).toFixed(6);
 //PPCProfit = ((PPCBalance * AvgPPC)-(PPCBalance * InitPPC)).toFixed(6);
 
-//Ammending HTML to the balance box section - will be a profitability calculator.
-//Quantity/Current Price/Price bought in at = +- profit infoboxes
-$('#balancebox').append('</br><table class=\"mylists\" style="font-size: 70%"><tr><td colspan=5><strong>Profit</strong></td></tr><tr><th></th><th>Balance</th><th>Currently</th><th>Bought @</th><th>Profit</th></tr><tr><td colspan=\"5\"></td></tr><tr><td>BTC</td><td class=\"BTCBalance\">64.48127185</td><td class=\"Current1\">TEST</td></tr><tr class=\"alt\"><td>PPC</td><td class=\"PPCBalance\">19,900.00000001</td><td class=\"Current2\">' + AvgPPC + '</td><td class=\"BoughtAt\">' + InitPPC + '</td><td class=\"Profit2\">' + PPCProfit + '</td></tr><tr><td>TRC</td><td class=\"TRCBalance\">4,975.00000000</td><td class=\"Current3\">' + AvgTRC + '</td><td class=\"BoughtAt3\">' + InitTRC + '</td><td class=\"Profit3\">' + TRCProfit + '</td></tr></table>');
 
 //Above made into a function
 //Could even probably merge this with the colouring of recent buy/sell.
 //Both use green/orange/red.
-function balancecolour(a,b) {
+balancecolour: function(a,b) {
 if(a > 0) {
   $(b).css({"background-color": "green"});
   } else if (a === 0){
@@ -107,7 +118,38 @@ if(a > 0) {
   $b).css({"background-color": "red"});
   }
 }
+};
 
-//Calling above function
-balancecolour(TRCProfit, ".Profit3"); //trcprofit
-balancecolour(PPCProfit, ".Profit2"); //ppcprofit
+
+//Detecting what type of coin you have in balance
+//There's probably a better way of doing this..meh.
+    for (var x = 2; x < 6; x++) 
+    {
+      if((BalanceBox.balanceType(x))==="") {
+        Break;
+      } else if ((BalanceBox.balanceType(x))==="PPC") {
+          //AvgPPC = Balancebox.AvgVal(11)
+          PPCBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="TRC") {
+          //AvgTRC = Balancebox.AvgVal(11)
+          TRCBalance = Balancebox.balance(x);
+      } else if ((BalanceBox.balanceType(x))==="LTC") {
+          //AvgLTC = Balancebox.AvgVal(11)
+          LTCBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="EUR") {
+          //AvgEUR = Balancebox.AvgVal(11)
+          EURBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="USD") {
+          //AvgUSD = Balancebox.AvgVal(11)
+          USDBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="NMC") {
+          //AvgNMC = Balancebox.AvgVal(11)
+          NMCBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="DVC") {
+          //AvgDVC = Balancebox.AvgVal(11)
+          DVCBalance = Balancebox.balance(x);        
+      } else if ((BalanceBox.balanceType(x))==="IXC") {
+          //AvgIXC = Balancebox.AvgVal(11)
+          IXCBalance = Balancebox.balance(x);        
+      }
+    }
