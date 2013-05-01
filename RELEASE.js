ed76b2e5e6991ed40eca96a7095bae8eefced1db
Mark Sleith <msleith@cngames.co.uk>
2013-05-01 22:31:10 +0100

    Working Framework

--- RELEASE.js
+++ RELEASE.js
@@ -12,168 +12,170 @@
 
 // When document is ready run thru our code.
 $(document).ready(function () {
+    // *************
+    // Our Object  *
+    // *************
+    var VA = (function () {
+        // Settings variables TODO: Put in JSON array object
+        var bColorOrderPriceCells, iRefreshRate;
+
+
+        // Initialise
+        var init = function () {
+            // Add JQUI css
+            $('head').append('<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/themes/redmond/jquery-ui.css" type="text/css" />');
+
+            InjectStyles();
+            CreateOptions();
+        },
+
+        // Creates an options interface
+        CreateOptions = function () {
+            // Create button
+            $('body').append('<div class="btnOptions"><span></span></div>');
+
+            // Create dialogue (this is where your options should go)
+            $('body').append('<div id="dialog" title="Options" style="display: none;">'
+                    + '<p style="margin-bottom:10px;">Select options below:</p>'
+                    // colorOrderPriceCells
+                    + '<p><input class="myCheckbox" id="colorOrderPriceCells" type="checkbox" value="true"/>'
+                    + '<label class="myCheckboxLabel" for="colorOrderPriceCells">Color the prices for order cells?</label> </p>'
+                    // refreshRate
+                    + '<p><label class="myCheckboxLabel" for="refreshRate">Refresh rate in ms. (Default: 90000)</label>'
+                    + '<input class="myInputBox" id="refreshRate" type="text" value="90000"/> </p>'
+                + '</div>');
+
+            // Initialise and set up on click listener for dialoge box
+            $("#dialog").dialog({
+                autoOpen: false,
+                minWidth: 800,
+                show: {
+                    effect: "blind",
+                    duration: 1000
+                },
+                hide: {
+                    effect: "explode",
+                    duration: 1000
+                },
+                buttons: [
+                    {
+                        text: "Save",
+                        click: function () {
+                            // Save our options
+                            SaveSettings();
+
+                            // Update UI
+                            UpdateUI();
+
+                            // Close the dialog box
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+
+            $(".btnOptions").click(function () {
+                $("#dialog").dialog("open");
+            });
+
+            InitSettings();
+            SaveSettings();
+        },
+
+        // Initialises settings values
+        InitSettings = function () {
+            $('#colorOrderPriceCells').attr('checked', GetBoolSetting('bColorOrderPriceCells'));
+            $('#refreshRate').attr('value', isNaN(GetIntSetting('iRefreshRate')) ? 90000 : GetIntSetting('iRefreshRate'));
+        },
+
+        // Saves options
+        SaveSettings = function () {
+            SetSetting("bColorOrderPriceCells", $('#colorOrderPriceCells').is(':checked'));
+            SetSetting("iRefreshRate", $('#refreshRate').attr("value"));
+        },
+
+        // Load options
+        LoadSettings = function () {
+            bColorOrderPriceCells = GetBoolSetting("bColorOrderPriceCells");
+            iRefreshRate = GetIntSetting("iRefreshRate");
+        },
+
+        // Updates all UI elements
+        UpdateUI = function () {
+            // (re)load options before setting UI elements
+            LoadSettings();
+
+            // Color cells [ RICKY: This line says if (bColorOrderPriceCells == true) then call function ColorOrderPriceCells(); ]
+            if (bColorOrderPriceCells) ColorOrderPriceCells();
+
+            //Refresh page every  90 secs
+            setTimeout("location.reload();", iRefreshRate);
+        },
+
+        // Injects styles to DOM
+        InjectStyles = function () {
+            $('head').append(
+                '<style>'
+                    // Options button
+                    + '.btnOptions {'
+                        + 'cursor: pointer;'
+                        + 'background: #ccc;'
+                        + 'border-radius: 10px;'
+                        + 'border: 1px solid #ddd;'
+                        + 'height: 25px; width: 25px;'
+                        + 'position: fixed; top: 5px; right: 5px;'
+                        + 'text-align: center;'
+                    + '}'
+                    // hover
+                    + '.btnOptions:hover {'
+                        + 'background: #bbb;'
+                    + '}'
+                    // Options button icon
+                    + '.btnOptions span {'
+                        + 'background-image: url("https://www.google.com/images/nav_logo123.png");'
+                        + 'background-position: -42px -259px;'
+                        + 'height: 17px; width: 17px; margin-top: 3px;'
+                        + 'display: inline-block; vertical-align: middle;'
+                    + '}'
+                    // Dialog checkboxes
+                    + '.myCheckbox {'
+                        + 'width: 25px; height: 25px;'
+                        + 'display: inline-block;'
+                        + 'padding: 0; margin: 0;'
+                        + 'vertical-align: middle;'
+                    + '}'
+                    // Dialoge checkbox labels
+                    + '.myCheckboxLabel {'
+                        + 'display: inline-block;'
+                        + 'margin: 5px;'
+                    + '}'
+                    // Text input box
+                    + '.myInputBox {'
+                        + 'width: 100px;'
+                    + '}'
+                + '</style>'
+            );
+        },
+        
+        // TODO 
+        ColorOrderPriceCells = function () {
+            alert("coloring cells");
+        }
+
+        //return true;
+        return {
+            init: init,
+            UpdateUI: UpdateUI
+        };
+
+    })();
+
     // Initialization
     VA.init();
 
     // Update UI.
     VA.UpdateUI();
 });
-
-
-var VA = (function () {
-    // Settings variables TODO: Put in JSON array object
-    var bColorOrderPriceCells, iRefreshRate;
-
-
-    // Initialise
-    var init = function () {
-        // TODO : DELETE ME
-        iRefreshRate = 90000;
-
-        // Add JQUI css
-        $('head').append('<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/themes/redmond/jquery-ui.css" type="text/css" />');
-
-        InjectStyles();
-        CreateOptions();
-    }
-
-    // Creates an options interface
-    var CreateOptions = function () {
-        // Create button
-        $('body').append('<div class="btnOptions"><span></span></div>');
-
-        // Create dialogue (this is where your options should go)
-        $('body').append('<div id="dialog" title="Options" style="display: none;">'
-                + '<p style="margin-bottom:10px;">Select options below:</p>'
-                // colorOrderPriceCells
-                + '<p><input class="myCheckbox" id="colorOrderPriceCells" type="checkbox" value="true"/>'
-                + '<label class="myCheckboxLabel" for="colorOrderPriceCells">Color the prices for order cells?</label> </p>'
-                // refreshRate
-                + '<p><label class="myCheckboxLabel" for="refreshRate">Refresh rate in ms. (Default: 90000)</label>'
-                + '<input class="myInputBox" id="refreshRate" type="text" value="90000"/> </p>'
-            + '</div>');
-
-        // Initialise and set up on click listener for dialoge box
-        $("#dialog").dialog({
-            autoOpen: false,
-            minWidth: 600,
-            show: {
-                effect: "blind",
-                duration: 1000
-            },
-            hide: {
-                effect: "explode",
-                duration: 1000
-            },
-            buttons: [
-                {
-                    text: "Save",
-                    click: function () {
-                        // Save our options
-                        SaveSettings();
-
-                        // Update UI
-                        UpdateUI();
-
-                        // Close the dialog box
-                        $(this).dialog("close");
-                    }
-                }
-            ]
-        });
-
-        $(".btnOptions").click(function () {
-            $("#dialog").dialog("open");
-        });
-
-        InitSettings();
-    }
-
-    // Initialises settings values
-    var InitSettings = function () {
-        $('#colorOrderPriceCells').attr('checked', GetBoolSetting('bColorOrderPriceCells'));
-    }
-
-    // Saves options
-    var SaveSettings = function () {
-        SetSetting("bColorOrderPriceCells", $('#colorOrderPriceCells').is(':checked'));
-    }
-
-    // Load options
-    var LoadSettings = function () {
-        bColorOrderPriceCells = GetBoolSetting("bColorOrderPriceCells");
-    }
-
-    // Updates all UI elements
-    var UpdateUI = function () {
-        // (re)load options before setting UI elements
-        LoadSettings();
-
-        // Color cells [ RICKY: This line says if (bColorOrderPriceCells == true) then call function ColorOrderPriceCells(); ]
-        if (bColorOrderPriceCells) ColorOrderPriceCells();
-
-        //Refresh page every  90 secs
-        setTimeout("location.reload();", iRefreshRate);
-    }
-
-    // Injects styles to DOM
-    var InjectStyles = function () {
-        $('head').append(
-            '<style>'
-                // Options button
-                + '.btnOptions {'
-                    + 'cursor: pointer;'
-                    + 'background: #ccc;'
-                    + 'border-radius: 10px;'
-                    + 'border: 1px solid #ddd;'
-                    + 'height: 25px; width: 25px;'
-                    + 'position: fixed; top: 5px; right: 5px;'
-                    + 'text-align: center;'
-                + '}'
-                // hover
-                + '.btnOptions:hover {'
-                    + 'background: #bbb;'
-                + '}'
-                // Options button icon
-                + '.btnOptions span {'
-                    + 'background-image: url("https://www.google.com/images/nav_logo123.png");'
-                    + 'background-position: -42px -259px;'
-                    + 'height: 17px; width: 17px; margin-top: 3px;'
-                    + 'display: inline-block; vertical-align: middle;'
-                + '}'
-                // Dialog checkboxes
-                + '.myCheckbox {'
-                    + 'width: 25px; height: 25px;'
-                    + 'display: inline-block;'
-                    + 'padding: 0; margin: 0;'
-                    + 'vertical-align: middle;'
-                + '}'
-                // Dialoge checkbox labels
-                + '.myCheckboxLabel {'
-                    + 'display: inline-block;'
-                    + 'margin: 5px;'
-                + '}'
-                // Text input box
-                + '.myInputBox {'
-                    + 'width: 100px;'
-                + '}'
-            + '</style>'
-        );
-    }
-
-
-
-
-
-    var ColorOrderPriceCells = function () {
-        alert("coloring cells");
-    }
-
-    return true;
-
-})();
-
-
 
 
 /******************
@@ -219,6 +221,11 @@ function GetBoolSetting(key) {
     }
 }
 
+// Gets a setting as type int
+function GetIntSetting(key) {
+    return parseInt(GetSetting(key));
+}
+
 // Check for HTML5 support for local storage?
 function hasLocalStorageSupport() {
     try {
@@ -227,3 +234,4 @@ function hasLocalStorageSupport() {
         return false;
     }
 }
+
